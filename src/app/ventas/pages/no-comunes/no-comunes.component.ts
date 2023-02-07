8b4a411d9069e005f6567b258319d2726b7e2d83
Charly021595 <charlydelacruzmartinez@gmail.com>
2023-02-06 18:59:30 -0600

    Actualizacion_2023_02_06

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -6,11 +7,86 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NoComunesComponent implements OnInit {
+export class NoComunesComponent {
 
-  constructor() { }
+  //i18nSelect
+  nombre: string = 'Carlos';
+  genero: string = 'masculino';
+  bandera: boolean = false;
+  bandera_if: boolean = true;
 
-  ngOnInit(): void {
+  invitacionMapa = {
+    'masculino': 'invitarlo',
+    'femenino' : 'invitarla'
   }
+
+  // i18nPlural
+  clientes: string[] = ['Maria', 'Mario', 'Rebeca'];
+  clientesMapa = {
+    '=0': 'No tenemos clientes esperando.',
+    '=1': 'tenemos # Cliente esperando.',
+    'other': 'tenemos # Clientes esperando.',
+  }
+
+  cambiarCliente(){
+    if (this.bandera) {
+      this.nombre = 'Carlos';
+      this.genero = 'masculino';
+      this.bandera = false;
+    }else{
+      this.nombre = 'Allyson';
+      this.genero = 'femenino';
+      this.bandera = true;
+    }
+  }
+
+  borrarCliente(){
+    if (this.clientes.length != 0) {
+      for (let i = 0; i < this.clientes.length; i++) {
+        this.clientes.splice(i, 1);
+        break;
+      }
+    }else{
+      this.bandera_if = false;
+    }
+    console.log(this.clientes);
+  }
+
+// KeyValue Pipe
+persona = {
+  nombre: 'Mariano',
+  edada: 40,
+  direccion: 'Ottawa, Canadá'
+}
+
+//Json Pipe
+heroes = [
+  {
+    nombre: 'Superman',
+    habilidades: 'Super fuerza, vuelo, visión de calor, aliento de hielo'
+  },
+  {
+    nombre: 'Batman',
+    habilidades: 'Artes Mariciales, Artilugios, Inteligencia'
+  },
+  {
+    nombre: 'Spiderman',
+    habilidades: 'Super fuerza, Dispara telaraña, Trepar por las paredes, Inteligencia'
+  },
+  {
+    nombre: 'Wolverine',
+    habilidades: 'Super fuerza, Regeneracion, Saca Garras, Cuerpo de Metal'
+  }
+]
+
+// Async Pipe
+miObservable = interval(5000);
+
+valorPromesa = new Promise( (resolve, reject) => {
+
+  setTimeout(() => {
+    resolve('Tenemos data de promesa');
+  }, 3500);
+});
 
 }
