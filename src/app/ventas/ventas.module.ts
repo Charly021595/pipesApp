5a35892d88d21745793feab3c5647cc26d4b256d
Charly021595 <charlydelacruzmartinez@gmail.com>
2023-02-07 00:10:36 -0600

    Actualizacion_2023_02_07_Mayuculas_pipe

--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -8,6 +8,9 @@ import { NoComunesComponent } from './pages/no-comunes/no-comunes.component';
 import { BasicosComponent } from './pages/basicos/basicos.component';
 import { OrdenarComponent } from './pages/ordenar/ordenar.component';
 
+import { MayusculasPipe } from './pipes/mayusculas.pipe';
+
+
 
 
 
@@ -16,7 +19,8 @@ import { OrdenarComponent } from './pages/ordenar/ordenar.component';
     NumerosComponent,
     NoComunesComponent,
     BasicosComponent,
-    OrdenarComponent
+    OrdenarComponent,
+    MayusculasPipe
   ],
   exports:[
     NumerosComponent,
