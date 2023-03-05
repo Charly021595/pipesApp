2a40b1c7c9080a5594b957318e09f4073c6cc842
CharlyMTZ <carlos.delacruz@arzyz.com>
2023-03-05 14:06:03 -0600

    Actualizacion_2023_03_05_CM

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -8,6 +8,7 @@ import {ButtonModule} from 'primeng/button';
 import {CardModule} from 'primeng/card';
 import {FieldsetModule} from 'primeng/fieldset';
 import {MenubarModule} from 'primeng/menubar';
+import {TableModule} from 'primeng/table';
 import {ToolbarModule} from 'primeng/toolbar';
 
 
@@ -18,6 +19,7 @@ import {ToolbarModule} from 'primeng/toolbar';
     CardModule,
     FieldsetModule,
     MenubarModule,
+    TableModule,
     ToolbarModule
   ]
 })
