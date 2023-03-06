d024ca823c581daf1cf9d80455713c85e91c713d
Charly021595 <charlydelacruzmartinez@gmail.com>
2023-03-05 22:33:14 -0600

    Actualizacion_2023_03_05_CM

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -9,9 +10,36 @@ import { Component } from '@angular/core';
 export class OrdenarComponent {
 
   enMayusculas: boolean = false;
+  heroes: Heroe[] = [
+    {
+      nombre: 'Spiderman',
+      vuela: false,
+      color: Color.azul
+    },
+    {
+      nombre: 'Daredevil',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'moon knight',
+      vuela: false,
+      color: Color.Blanco
+    },
+    {
+      nombre: 'Iron Man',
+      vuela: true,
+      color: Color.rojo
+    }
+  ]
 
   enMayusculas_click(){
     return ( this.enMayusculas ) ? this.enMayusculas = false : this.enMayusculas = true;
-  } 
+  }
 
 }
